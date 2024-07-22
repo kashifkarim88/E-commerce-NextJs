640d1d3ebf3ee4c2437885ff76f150ac61467e92
Kashif Karim <50681010+kashifkarim88@users.noreply.github.com>
2024-07-22 12:16:32 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
     env: {
         STRIPE_PUBLIC_KEY: 'pk_test_51PWubIRoHKATZDpCOGMrmyHmstbLzRUjRC1ED05OY5ZkAiwi5vVohDcYrpQ2g3b5lUE3McJb38r27Cw4wnKz2S2G003ueD7d84',
         STRIPE_PRIVATE_KEY: 'sk_test_51PWubIRoHKATZDpCy9BMxAiOUqzRb3lFb5pAG25VzzqGDR98zUOSICwAAK2189iwkL2On65EylC0OJ2M6Gho9VQG00RdFbgNmD',
-        STRIPE_WEBHOOK_SECRECT: 'whsec_5726837d4c7f4827e447fed88ada0190b05de2eb054439d117928d4fe3e69ebb'
+        STRIPE_WEBHOOK_SECRECT: 'whsec_5726837d4c7f4827e447fed88ada0190b05de2eb054439d117928d4fe3e69ebb',
+        MONGODB_URI: 'mongodb+srv://kashifkarimkhan88:NextJsFullStackEcommerce@nextjs.kb6lose.mongodb.net/?retryWrites=true&w=majority&appName=NextJs'
     },
     images: {
         remotePatterns: [
